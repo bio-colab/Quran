a3d580f01077fd916ad6aab81f48695dc2ad08d9
Eylias Sharar <59902143+bio-colab@users.noreply.github.com>
2025-11-02 10:56:47 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'Eylias.Quran.app',
+  appName: 'Quran2',
+  webDir: 'dist'
+};
+
+export default config;
